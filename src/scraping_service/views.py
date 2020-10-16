78e45ef2e941351992ac70d7c27f212f32746b7a
zemags <azmt2008@gmail.com>
2020-10-16 00:05:27 -0700

    Project create

---
+++ src/scraping_service/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+import datetime
+
+def home(request):
+    date = datetime.datetime.now().date()
+    name = 'John'
+    context = {'date': date, 'name': name}
+    return render(request, 'home.html', context)
