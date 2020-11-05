66b78555dd3d395ab08e3838ca4c166bcaeee518
zemags <azmt2008@gmail.com>
2020-11-04 22:39:13 -0800

    Add errors sender

--- src/send_emails.py
+++ src/send_emails.py
@@ -1,5 +1,6 @@
 import os, sys
 from collections import defaultdict
+import datetime
 
 from django.core.mail import EmailMultiAlternatives
 
@@ -12,16 +13,17 @@ import django
 django.setup()
 #
 
-from scraping.models import Vacancy
+from scraping.models import Vacancy, Error, URL, Url
 from scraping_service.settings import EMAIL_HOST_USER
 
-
+ADMIN_USER = ''
 User = get_user_model()
 qs = User.objects.filter(send_email=True).values('city', 'language', 'email')  # get all values from db
 empty = '<h2>No vacancies</h2>'
+today = datetime.date.today()
 
+users_dict = {}
 if qs.exists():
-    users_dict = {}
     params = defaultdict(list)
     vacancies = defaultdict(list)
     for query in qs:
@@ -29,7 +31,7 @@ if qs.exists():
         params['city_id__in'].append(query['city'])
         params['language_id__in'].append(query['language'])
 
-    qs_vacancies = Vacancy.objects.filter(**params).values()
+    qs_vacancies = Vacancy.objects.filter(**params, timestamp=today).values()
     if qs_vacancies.exists():
         for vacancy in qs_vacancies:
             vacancies[(vacancy['city_id'], vacancy['language_id'])].append(vacancy)
@@ -37,13 +39,46 @@ if qs.exists():
             rows = vacancies.get(keys, [])
             html = ''
             for row in rows:
-                html += '<a href="{url}">{title}</a>'.format(url=row['url'], title=row['title'])
+                html += '<a href="{url}">{title}</a><br>'.format(url=row['url'], title=row['title'])
             html_ = html if html else empty
             for email in emails:
                 msg = EmailMultiAlternatives(
-                    subject='Vacancy sender',
+                    subject='Vacancy sender from %s' % today,
                     body='Vacancy sender',
                     from_email=EMAIL_HOST_USER,
                     to=[email])
                 msg.attach_alternative(html_, 'text/html')
                 msg.send()
+
+qs = Error.objects.filter(timestamp=today)
+if qs.exists():
+    error = qs.first()
+    data = error.data
+    html = ''
+    for er in data:
+        html += '<a href="{url}">{title}</a><br>'.format(url=er['url'], title=er['title'])
+    html_ = html if html else 'No errors'
+    msg = EmailMultiAlternatives(
+        subject='Errors sender from %s' % today,
+        body='Errors sender',
+        from_email=EMAIL_HOST_USER,
+        to=[ADMIN_USER]
+    )
+    msg.attach_alternative(html_, 'text/html')
+    msg.send()
+
+qs = Url.objects.all().values('city', 'language')
+urls_dict = {(i['city'], i['language']): True for i in qs}
+urls_err = ''
+for keys in users_dict.keys():
+    if not urls_dict.get(keys):
+        urls_err += 'for {city} and {language} there is no urls<br>'.format(city=keys[0], language=keys[0])
+if urls_err:
+    msg = EmailMultiAlternatives(
+        subject='Urls error sender from %s' % today,
+        body='Urls errors sender',
+        from_email=EMAIL_HOST_USER,
+        to=[ADMIN_USER]
+    )
+    msg.attach_alternative(html_, 'text/html')
+    msg.send()
