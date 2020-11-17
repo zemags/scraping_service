82964ce3b693f0823d5ef997dbb849235661e13f
zemags <azmt2008@gmail.com>
2020-11-17 06:36:07 -0800

    Add class based views, reformat htmls

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView, ListView
 
 from .forms import FindForm
 from .models import Vacancy
@@ -35,3 +36,51 @@ def list_view(request):
 
     context['object_list'] = page_obj
     return render(request, 'scraping/list.html', context)
+
+
+def v_detail(request, pk):
+    #obj = Vacancy.objects.get(pk=pk)  # by primary keyfrom request find in BD
+    obj = get_object_or_404(Vacancy, pk=pk)
+    return render(request, 'scraping/detail.html',{'object': obj})
+
+
+class VacancyDetail(DetailView):
+    # class based view
+    # redefine attrs
+    queryset = Vacancy.objects.all()
+    template_name = 'scraping/detail.html'
+    context_object_name = 'object'  # to get access in html file by this name or other you want
+
+
+class VacancyList(ListView):
+    model = Vacancy
+    template_name = 'scraping/list.html'
+    form = FindForm()
+
+    paginate_by = 5  # define pagination in classbased view
+    #  to show pagination in html the name must be page_obj
+
+    def get_context_data(self, **kwargs):  # add to html context['object_list']  and add smth to context
+        context = super().get_context_data(**kwargs)
+        context['city'] = self.request.GET.get('city')
+        context['language'] = self.request.GET.get('language')
+
+        context['form'] = self.form  #  pass form to context
+
+        return context
+
+    def get_queryset(self):  # redefine  queryset like Model_name.objects.all()
+        city = self.request.GET.get('city')
+        language = self.request.GET.get('language')
+        if city or language:
+            filter_dict = dict()
+            # two underscore to get acccess to models table
+            if city:
+                filter_dict['city__slug'] = city.lower()
+            if language:
+                filter_dict['language__slug'] = language.lower()
+            qs = Vacancy.objects.filter(**filter_dict)
+        else:
+            qs = Vacancy.objects.all()
+
+        return qs
--- src/scraping_service/urls.py
+++ src/scraping_service/urls.py
@@ -15,13 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from scraping.views import home_view, list_view
+from scraping.views import home_view, list_view, v_detail, VacancyDetail, VacancyList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('list/', list_view, name='list'),
+    # path('list/', list_view, name='list'),
+    path('list/', VacancyList.as_view(), name='list'),
 
     path('accounts/', include(('accounts.urls', 'accounts'))),
+    # path('detail/<int:pk>/', v_detail, name='detail'),
+    path('detail/<int:pk>/', VacancyDetail.as_view(), name='detail'),
 
     path('', home_view, name='home'),
 ]
